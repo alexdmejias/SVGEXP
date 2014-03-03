f01bc7d90d9be087e2f3d0e6e39b3bf48089f294
alex mejias <alexdmejias@gmail.com>
2014-03-03 10:41:04 -0500

    cleanup and comments

--- script.js
+++ script.js
@@ -1,172 +1,79 @@
-width = document.body.clientWidth;
-l = 10;
-p = 5;
 
-q = 20;
-line_anim_duration = 50;
-new_anim_delay = 50;
-cordinates_used = [];
-remaining_cordinates = [];
-var timer;
-width = (l*q)+((q-1)*p);
-var shape_length;
+var l = 10, // length of shape side
+	p = 5, // padding per shape
+	q = 20, // quantity per axis
+	container_width = (l*q)+((q-1)*p), // width of the whole container
+	line_anim_duration = 50, // line animation duration
+	new_anim_delay = 50, // time between new squares
+	cordinates_used = [],
+	remaining_cordinates = [],
+	timer,
+	shape_length,
+	parent_div = document.getElementsByClassName('container')[0];
+	svg_elem = document.getElementsByTagName('svg')[0];
+	draw = SVG(svg_elem).fixSubPixelOffset();
+	group = draw.group();
 
-
-parent_div = document.getElementsByClassName('container')[0];
-
-parent_div.style.width = width + 1 + "px";
-parent_div.style.height = width + 1 + "px";
-
-svg_elem = document.getElementsByTagName('svg')[0];
-
-var draw = SVG(svg_elem).fixSubPixelOffset();
-group = draw.group();
-/*function randomCords(hMax, vMax) {
-	return Array(Math.floor(Math.random() * hMax), Math.floor(Math.random() * vMax))
-}*/
-
+// generates an array of cordinates for the loop to pick from
 function genGrid(x,y) {
 	for(var i = 0; x > i; i++) {
 		for(var h = 0; y > h; h++) {
-			remaining_cordinates.push(Array(i,h))
+			remaining_cordinates.push([i,h])
 		}
 	}
 }
 
-
+// given an X and Y, it will create a square, it will then animate its stroke
 function createSquare(x, y) {
+		// clear the previous timer, should help with performance
 		window.clearTimeout(timer);
 
-		// var cords_picked = x+''+y;
-		// console.warn(cords_picked)
-		// if (cordinates_used.indexOf(cords_picked) < 0) {
-			group.path('M '+(((l*x)+(p*x))+1)+' '+((l*y)+(p*y)+1)+'H '+((l*(x+1))+(p*x))+' V ' + ((l*(y+1))+(p*y))+'H '+(((l*x)+(p*x))+1)+'Z').attr({
-				'fill': '#fff',
-				'stroke' : 'red',
-				'strokeWidth': 1
-			})
+		// make the path
+		group.path('M '+(((l*x)+(p*x))+1)+' '+((l*y)+(p*y)+1)+'H '+((l*(x+1))+(p*x))+' V ' + ((l*(y+1))+(p*y))+'H '+(((l*x)+(p*x))+1)+'Z');
 
-			if (typeof shape_length == "undefined"){
-				shape_length = group.first().length();
-			}
+		// define the length of the shape incase it hasnt already
+		if (typeof shape_length == "undefined"){
+			shape_length = group.first().length();
+		}
 
-			group.last().attr({
-				'stroke-dasharray' : shape_length,
-				'stroke-dashoffset': shape_length
-			}).animate(line_anim_duration).attr({
-				'stroke-dashoffset': 0
-			});
-			// cordinates_used.push(cords_picked);
-		// } else {
-			// cords_to_use = randomCords(10,10);
-			// createSquare(cords_to_use[0], cords_to_use[1]);
-			console.info('new square')
-		// }
-		timer = setTimeout(kickoff, new_anim_delay);
+		group.last().attr({
+			'fill': '#fff',
+			'stroke' : 'red',
+			'strokeWidth': 1,
+			'stroke-dasharray' : shape_length,
+			'stroke-dashoffset': shape_length
+		}).animate(line_anim_duration).attr({
+			'stroke-dashoffset': 0
+		});
+
+		// start a new timer for the next shape
+		timer = setTimeout(animationStart, new_anim_delay);
 }
 
-function kickoff() {
-	// var	r = randomCords(10,10);
-	// if (remaining_cordinates.length < 1) {
-	// 	genGrid(q,q)
-	// }
+function animationStart() {
 	if (group.children().length < (q*q) ){
 		r = Math.floor(Math.random() * remaining_cordinates.length);
-		console.log(r);
 		createSquare(remaining_cordinates[r][0], remaining_cordinates[r][1])
+
+		// remove the array element that was just used
 		remaining_cordinates.splice(r,1);
 	} else {
-		stop();
+		// stop animation if there are no more cordinates left
+		animationStop();
 	}
-}
-function init() {
-	if (remaining_cordinates.length < 1) {
-		genGrid(q,q)
-	}
-	kickoff()
 }
 
-function stop() {
+function animationStop() {
 	window.clearTimeout(timer);
 }
 
+// style the parent box and start the whole animation
+function init() {
+	parent_div.style.width = container_width + 1 + "px";
+	parent_div.style.height = container_width + 1 + "px";
+
+	genGrid(q,q)
+	animationStart()
+}
+
 init();
-
-// function animate(i,x,y) {
-	// xPos = l * x
-	// group.path('M1 1H '+((l*(i+1))+(p*i))+'V '+((l*(h+1))+(p*h))+'H '+(((l*i)+(p*i))+1)+'Z').attr({
-	// 	'fill': '#ccc',
-	// 	'stroke' : 'red',
-	// 	'strokeWidth': 5
-	// });
-	// group.last().animate(1000).attr({
-	// 	'stroke-dashoffset': 0
-	// });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*squares_left = [];
-for (var i = 0; 30 > i; i ++) {
-	for(var h = 0; 30 > h; h++) {
-		group.path('M'+(((l*i)+(p*i))+1)+' '+ ((l*h)+(p*h)+1) +'H '+((l*(i+1))+(p*i))+'V '+((l*(h+1))+(p*h))+'H '+(((l*i)+(p*i))+1)+'Z').attr({
-			'fill': '#fff',
-			'stroke' : 'red',
-			'strokeWidth': 5
-		});
-	}
-
-	squares_left.push(i);
-}
-var length = group.first().length()
-group.each(function() {
-	this.attr({
-	'stroke-dasharray' : length,
-	'stroke-dashoffset': length
-	})
-});
-
-
-function animate(i) {
-	group.get(i).animate(1000).attr({
-		'stroke-dashoffset': 0
-	})
-}
-
-timer = setInterval(function() {
-	if (squares_left.length > 0) {
-		random = Math.floor(Math.random() * squares_left.length)
-		animate(squares_left[random]);
-
-		squares_left.splice(random, 1)
-	} else {
-		console.log('clearning timer');
-		window.clearInterval(timer);
-	}
-}, 1000);
-*/
