6aba9867c64ea1361cb40453d93109f0ae01e3a2
alex mejias <alexdmejias@gmail.com>
2014-04-01 16:38:49 -0400

    adding modal module, it will serve as the container for the info and controls; more info event;

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,38 +1,50 @@
-define([], function() {
+define([], function () {
 	'use strict';
 
-	return function(app) {
+	return function (app) {
 		return {
-			getViewportSize: function() {
+			onResize: function () {
+			},
+
+			getViewportSize: function () {
 				app.viewportSize = {
 					width: document.documentElement.clientWidth,
 					height: document.documentElement.clientHeight
 				};
 			},
 
-			demoSwitch: function() {
-				app.buttons.addEventListener('click', function(e) {
-					if(e.target.tagName == 'A') {
+			demoSwitch: function () {
+				app.buttons.addEventListener('click', function (e) {
+					if (e.target.tagName === 'A') {
 
 						var currentDemo = e.target.getAttribute('data-demo');
 
 						app.priorAnimation = app.currentAnimation;
 
-						if ((app.priorAnimation !== currentDemo) && (app.priorAnimation !== '')) {
-							app.animations[app.priorAnimation].reset();
-						}
+						app.animations[app.priorAnimation].reset();
 
 						document.getElementsByTagName('body')[0].className = currentDemo;
 
 						app.animations[currentDemo].init();
 						app.currentAnimation = currentDemo;
+
 					}
 				});
 			},
 
-			init : function() {
+			moreInfo: function () {
+				var infoButton = document.getElementById('info');
+				infoButton.addEventListener('click', function (e) {
+					e.preventDefault();
+					app.modal.show().text(['Please excuse the apperance while I keep working on this project.',
+						'What is this? A series of SVG elements, please look at the GitHub repo to find what else is coming to this project']);
+				});
+			},
+
+			init : function () {
 				this.getViewportSize();
 				this.demoSwitch();
+				this.moreInfo();
 			}
 		};
 	};
---
+++ assets/js/modal.js
@@ -0,0 +1,68 @@
+'use strict';
+define([], function () {
+	return {
+		created: false,
+		visible: false,
+		bgElem: null,
+		fgElem: null,
+		create: function () {
+			this.bgElem = document.createElement('div');
+			this.bgElem.setAttribute('id', 'modalBackground');
+			document.getElementsByTagName('body')[0].appendChild(this.bgElem);
+
+			this.fgElem = document.createElement('div');
+			this.fgElem.setAttribute('id', 'modalForeground');
+			document.getElementById('modalBackground').appendChild(this.fgElem);
+
+		},
+
+		events: function () {
+			var elem = this.bgElem;
+			var hide = function () {
+				elem.className = '';
+			};
+
+			this.bgElem.addEventListener('click', hide);
+
+		},
+
+		init: function () {
+			if (!this.created) {
+				this.create();
+				this.created = true;
+			}
+
+			this.events();
+		},
+
+		clear: function () {
+			while (this.fgElem.firstChild) {
+				this.fgElem.removeChild(this.fgElem.firstChild);
+			}
+
+			return this;
+		},
+
+		show: function () {
+			this.init();
+			this.bgElem.className = 'showing';
+			return this;
+		},
+
+		text: function (text) {
+			this.clear();
+
+			if (typeof(text) === 'string') {
+				this.fgElem.innerHTML = '<p>' + text + '</p>';
+			} else {
+				for (var par in text ) {
+					var p = document.createElement('p');
+					p.innerHTML = text[par];
+					this.fgElem.appendChild(p);
+				}
+			}
+
+			return this;
+		}
+	};
+});
