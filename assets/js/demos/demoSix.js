ba3c9db9b07cb6b782f388b2a995cfa7b0955b6f
alex mejias <alexdmejias@gmail.com>
2014-04-25 20:26:51 -0400

    Six: cleanup + better var names;

--- assets/js/demos/demoSix.js
+++ assets/js/demos/demoSix.js
@@ -2,21 +2,23 @@ define(['../helpers'], function (h) {
 	'use strict';
 	function demo(app) {
 
-		var center = 250,
-			sideLength = 30,
-			sideLengthHalf = sideLength / 2,
-			loopTimer = 50, // timer between each iteration
-			padding = 200, // padding for the squares from the center of the grid
+		var imageSize = 500,
+			center = imageSize / 2,
+			rangeSize = 10,
+			rangeSizeHalf = rangeSize / 2,
+			loopTimer = 20, // timer between each iteration
+			padding = 200, // padding for the squareGuides from the center of the grid to the square
 			initited = false,
 			squareStyle = {
 				fill: 'none',
-				stroke: 'red'
+				stroke: 'none'
 			},
 			polygonStyle = {
 				opacity: 0.25,
 				fill: 'red'
 			},
 			numPolygons = 10, // number of polygons to make
+			numVertices = 4,
 			polygons = [],
 			ranges = [],
 			globalTimer = null;
@@ -34,8 +36,8 @@ define(['../helpers'], function (h) {
 
 		function reset(type) {
 			app.parentGroup.clear();
-			h.stopTimer(globalTimer)
-			if(type && type === 'hard') {
+			h.stopTimer(globalTimer);
+			if (type && type === 'hard') {
 				initited = false;
 			}
 		}
@@ -50,16 +52,16 @@ define(['../helpers'], function (h) {
 
 		function setup() {
 
-			var squares = [
-				app.parentGroup.rect(sideLength, sideLength).style(squareStyle).move(center - sideLengthHalf,center - (sideLength + padding) ),
-				app.parentGroup.rect(sideLength, sideLength).style(squareStyle).move(center + padding,center - sideLengthHalf),
-				app.parentGroup.rect(sideLength, sideLength).style(squareStyle).move(center - sideLengthHalf,center + padding),
-				app.parentGroup.rect(sideLength, sideLength).style(squareStyle).move(center - (sideLength + padding) ,center - sideLengthHalf)
+			var squareGuides = [
+				app.parentGroup.rect(rangeSize, rangeSize).style(squareStyle).move(center - rangeSizeHalf,center - (rangeSize + padding) ),
+				app.parentGroup.rect(rangeSize, rangeSize).style(squareStyle).move(center + padding,center - rangeSizeHalf),
+				app.parentGroup.rect(rangeSize, rangeSize).style(squareStyle).move(center - rangeSizeHalf,center + padding),
+				app.parentGroup.rect(rangeSize, rangeSize).style(squareStyle).move(center - (rangeSize + padding) ,center - rangeSizeHalf)
 			];
 
-			// get the ranges of each of the squares
+			// get the ranges of each of the squareGuides
 			for (var i = 0; i < 4; i++) {
-				var currSquare = squares[i].bbox();
+				var currSquare = squareGuides[i].bbox();
 				ranges.push([[currSquare.x, currSquare.cx + (currSquare.width / 2) ], [currSquare.y, currSquare.cy + (currSquare.height / 2)]]);
 			}
 		}
@@ -70,30 +72,29 @@ define(['../helpers'], function (h) {
 		 */
 		function renderPolygons() {
 			for (var i = 0; i < numPolygons; i++) {
-				var polygon = app.parentGroup.polygon(genPoints(ranges)).style(polygonStyle);
+				var polygon = app.parentGroup.polygon(genVertices(ranges)).style(polygonStyle);
 				polygons.push(polygon);
 			}
 		}
 
 		/**
-		 * returns an array of randomly generated points
+		 * returns an array of randomly generated vertices
 		 * @param  {array} ranges array with arrays of min/max values for each of the poaints
 		 * @return {[array]} a one dimensinal array with the final coordinates for the polygon
 		 */
-		function genPoints(ranges) {
+		function genVertices(ranges) {
 			// houses the cordinates of an array with x1, y1, x2, y,2.... coordinates
-			var points = [];
-			for (var i = 0; i < 4; i++) {
-				points.push(h.genRandom(ranges[i][0][0], ranges[i][0][1]), h.genRandom(ranges[i][1][0], ranges[i][1][1]))
+			var vertices = [];
+			for (var i = 0; i < numVertices; i++) {
+				vertices.push(h.genRandom(ranges[i][0][0], ranges[i][0][1]), h.genRandom(ranges[i][1][0], ranges[i][1][1]));
 			}
-			return [points];
+			return [vertices];
 		}
 
 		return {
 			init: init,
-			reset: reset,
-			app: app
-		}
+			reset: reset
+		};
 
 	}
 
