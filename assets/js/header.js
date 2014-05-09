2642550bd3a35a3c2193d349fa046729b8b2584e
alex mejias <alexdmejias@gmail.com>
2014-05-08 23:17:37 -0400

    cleaned and refactored header

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,20 +1,51 @@
 define(['eventsManager'],
 	function (E) {
 
-		function init() {
-			// Find a better way to do this
-			var demoButtons = document.getElementsByClassName('demos')[0];
+		function demoButtons(event) {
+			var currentDemo = event.target.textContent;
 
-			demoButtons.addEventListener('click', function (e) {
-				if (e.target.tagName === 'BUTTON') {
-
-					var currentDemo = e.target.textContent;
-
-					E.publish('app/demoSwitch', {
-						selectedDemo: currentDemo
-					});
-				}
+			E.publish('app/demoSwitch', {
+				selectedDemo: currentDemo
 			});
+		}
+
+		function info(event) {
+			event.preventDefault();
+
+			E.publish('modal/show', {
+				template: 'more-info'
+			});
+
+			ga('send', 'event', 'category', 'more_info');
+		}
+
+		function controls() {
+			console.info('this will house the controls');
+		}
+
+		function init() {
+			var buttons = document.getElementsByTagName('nav')[0];
+
+			buttons.addEventListener('click', function(e) {
+
+				switch(e.target.parentNode.className) {
+					case 'controls':
+						controls(e);
+						break;
+					case 'demos':
+						console.log('demos');
+						demoButtons(e);
+						break;
+					case 'title':
+						info(e);
+						console.log('title');
+						break;
+					default:
+						console.log('huh?');
+				}
+
+			});
+
 		}
 
 		return {
